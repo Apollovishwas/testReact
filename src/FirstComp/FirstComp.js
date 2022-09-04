27c8b2aa356a1bb88458e5ee26431cafb752f8e2
Vee <apollovishwas@yahoo.com>
2022-09-04 13:33:45 -0400

    first commit

---
+++ src/FirstComp/FirstComp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './FirstComp.css'
+const FirstComp = () => {
+    return (
+        <div>
+            hello arun is gay 
+            <button>click here</button>
+        </div>
+    )
+};
+
+export default FirstComp;
